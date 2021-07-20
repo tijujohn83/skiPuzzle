1c711c4d553cfc33f312084e51cdbc32a9e9da39
Tiju John (TJ) <tjoh@simcorp.com>
2021-07-20 14:21:55 +0200

    debugger display

--- LandScapeCell.cs
+++ LandScapeCell.cs
@@ -1,6 +1,9 @@
 ﻿
+using System.Diagnostics;
+
 namespace Problem1
 {
+    [DebuggerDisplay("{X}, {Y}, {Z}, {LengthFromPeak}, {IsPeak.hasValue ? IsPeak.Value.ToString() : string.Empty}")]
     public struct LandScapeCell
     {
         public int X;
@@ -8,7 +11,6 @@ namespace Problem1
         public int Z;
         public int LengthFromPeak;
         public bool? IsPeak;
-
 
         public LandScapeCell(int x, int y, int z, int lengthFromPeak)
         {
