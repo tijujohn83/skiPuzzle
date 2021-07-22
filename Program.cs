3c8bb0a7ee0fb3480e9b6ef51d36fe798da90e32
Tiju John (TJ) <tjoh@simcorp.com>
2021-07-22 12:50:31 +0200

    still some difference

--- Program.cs
+++ Program.cs
@@ -42,73 +42,73 @@ namespace Problem1
             sb.Append("Cells = ").Append(string.Join("🡢", solution.Path.Select(node => $"[{node.X}, {node.Y}]"))).AppendLine();
             sb.Append("Heights = ").Append(string.Join("🡢", solution.Path.Select(node => LandScapeMatrix.Cells[node.X, node.Y].Z))).AppendLine().AppendLine();
 
-            sb.AppendLine("Input");
-            for (var x = 0; x < LandScapeMatrix.SquareMapSide; x++)
-            {
-                for (var y = 0; y < LandScapeMatrix.SquareMapSide; y++)
-                {
-                    sb.Append($"{LandScapeMatrix.Cells[x, y].Z}".PadLeft(Space).PadRight(Space + 2));
-                }
-                sb.AppendLine();
-            }
+            //sb.AppendLine("Input");
+            //for (var x = 0; x < LandScapeMatrix.SquareMapSide; x++)
+            //{
+            //    for (var y = 0; y < LandScapeMatrix.SquareMapSide; y++)
+            //    {
+            //        sb.Append($"{LandScapeMatrix.Cells[x, y].Z}".PadLeft(Space).PadRight(Space + 2));
+            //    }
+            //    sb.AppendLine();
+            //}
 
-            sb.AppendLine().AppendLine("Peaks");
-            for (var x = 0; x < LandScapeMatrix.SquareMapSide; x++)
-            {
-                for (var y = 0; y < LandScapeMatrix.SquareMapSide; y++)
-                {
-                    var isPeak = LandScapeMatrix.Cells[x, y].IsPeak;
-                    sb.Append(isPeak != null && isPeak.Value ? LandScapeMatrix.Cells[x, y].Z.ToString().PadLeft(Space).PadRight(Space + 2) : "●".PadLeft(Space).PadRight(Space + 2));
-                }
-                sb.AppendLine();
-            }
+            //sb.AppendLine().AppendLine("Peaks");
+            //for (var x = 0; x < LandScapeMatrix.SquareMapSide; x++)
+            //{
+            //    for (var y = 0; y < LandScapeMatrix.SquareMapSide; y++)
+            //    {
+            //        var isPeak = LandScapeMatrix.Cells[x, y].IsPeak;
+            //        sb.Append(isPeak != null && isPeak.Value ? LandScapeMatrix.Cells[x, y].Z.ToString().PadLeft(Space).PadRight(Space + 2) : "●".PadLeft(Space).PadRight(Space + 2));
+            //    }
+            //    sb.AppendLine();
+            //}
 
-            sb.AppendLine().AppendLine("Solution");
+            //sb.AppendLine().AppendLine("Solution");
 
-            for (var x = 0; x < LandScapeMatrix.SquareMapSide; x++)
-            {
-                for (var y = 0; y < LandScapeMatrix.SquareMapSide; y++)
-                {
-                    var node = solution.Path.FirstOrDefault(p => p.X == x && p.Y == y);
-                    if (node != null)
-                    {
-                        var nodeIndex = solution.Path.IndexOf(node);
-                        var isLastCell = nodeIndex == solution.Path.Count - 1;
+            //for (var x = 0; x < LandScapeMatrix.SquareMapSide; x++)
+            //{
+            //    for (var y = 0; y < LandScapeMatrix.SquareMapSide; y++)
+            //    {
+            //        var node = solution.Path.FirstOrDefault(p => p.X == x && p.Y == y);
+            //        if (node != null)
+            //        {
+            //            var nodeIndex = solution.Path.IndexOf(node);
+            //            var isLastCell = nodeIndex == solution.Path.Count - 1;
 
-                        var start = nodeIndex == 0 ? "@" : "";
+            //            var start = nodeIndex == 0 ? "@" : "";
 
-                        if (!isLastCell)
-                        {
-                            var next = solution.Path[nodeIndex + 1];
+            //            if (!isLastCell)
+            //            {
+            //                var next = solution.Path[nodeIndex + 1];
 
-                            if (next.X == node.X && next.Y < node.Y)
-                                sb.Append($"{start}🡠".PadLeft(Space + 1).PadRight(Space + 3));
-                            else if (next.X == node.X && next.Y > node.Y)
-                                sb.Append($"{start}🡢".PadLeft(Space + 1).PadRight(Space + 3));
-                            else if (next.X < node.X && next.Y == node.Y)
-                                sb.Append($"{start}🡡".PadLeft(Space + 1).PadRight(Space + 3));
-                            else if (next.X > node.X && next.Y == node.Y)
-                                sb.Append($"{start}🡣".PadLeft(Space + 1).PadRight(Space + 3));
-                            else if (next.X < node.X && next.Y < node.Y)
-                                sb.Append($"{start}🡤".PadLeft(Space + 1).PadRight(Space + 3));
-                            else if (next.X < node.X && next.Y > node.Y)
-                                sb.Append($"{start}🡥".PadLeft(Space + 1).PadRight(Space + 3));
-                            else if (next.X > node.X && next.Y > node.Y)
-                                sb.Append($"{start}🡦".PadLeft(Space + 1).PadRight(Space + 3));
-                            else if (next.X > node.X && next.Y < node.Y)
-                                sb.Append($"{start}🡧".PadLeft(Space + 1).PadRight(Space + 3));
-                        } else
-                        {
-                            sb.Append("@".PadLeft(Space).PadRight(Space + 2));
-                        }
-                    } else
-                    {
-                        sb.Append("●".PadLeft(Space).PadRight(Space + 2));
-                    }
-                }
-                sb.AppendLine();
-            }
-            sb.AppendLine("------------------------------------------------------------------------------------------------------");
+            //                if (next.X == node.X && next.Y < node.Y)
+            //                    sb.Append($"{start}🡠".PadLeft(Space + 1).PadRight(Space + 3));
+            //                else if (next.X == node.X && next.Y > node.Y)
+            //                    sb.Append($"{start}🡢".PadLeft(Space + 1).PadRight(Space + 3));
+            //                else if (next.X < node.X && next.Y == node.Y)
+            //                    sb.Append($"{start}🡡".PadLeft(Space + 1).PadRight(Space + 3));
+            //                else if (next.X > node.X && next.Y == node.Y)
+            //                    sb.Append($"{start}🡣".PadLeft(Space + 1).PadRight(Space + 3));
+            //                else if (next.X < node.X && next.Y < node.Y)
+            //                    sb.Append($"{start}🡤".PadLeft(Space + 1).PadRight(Space + 3));
+            //                else if (next.X < node.X && next.Y > node.Y)
+            //                    sb.Append($"{start}🡥".PadLeft(Space + 1).PadRight(Space + 3));
+            //                else if (next.X > node.X && next.Y > node.Y)
+            //                    sb.Append($"{start}🡦".PadLeft(Space + 1).PadRight(Space + 3));
+            //                else if (next.X > node.X && next.Y < node.Y)
+            //                    sb.Append($"{start}🡧".PadLeft(Space + 1).PadRight(Space + 3));
+            //            } else
+            //            {
+            //                sb.Append("@".PadLeft(Space).PadRight(Space + 2));
+            //            }
+            //        } else
+            //        {
+            //            sb.Append("●".PadLeft(Space).PadRight(Space + 2));
+            //        }
+            //    }
+            //    sb.AppendLine();
+            //}
+            //sb.AppendLine("------------------------------------------------------------------------------------------------------");
 
         }
 
